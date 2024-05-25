d3652632c02452fa173d6c4bbf831dc73433f156
MariamKatamashvili <katamashvili.mariam@kiu.edu.ge>
2024-05-25 20:04:27 +0400

    Add security

--- src/main/java/com/mariamkatamashvili/trainerwork/security/JwtTokenProvider.java
+++ src/main/java/com/mariamkatamashvili/trainerwork/security/JwtTokenProvider.java
@@ -1,55 +1,55 @@
-//package com.mariamkatamashvili.trainerwork.security;
-//
-//import io.jsonwebtoken.Claims;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.security.Keys;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//
-//import javax.crypto.SecretKey;
-//import java.util.Base64;
-//import java.util.Date;
-//import java.util.function.Function;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class JwtTokenProvider {
-//    @Value("${security.jwt.secret}")
-//    private String secret;
-//
-//    public String extractUsername(String token) {
-//        return extractClaim(token, Claims::getSubject);
-//    }
-//
-//    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-//        final Claims claims = extractAllClaims(token);
-//        return claimsResolver.apply(claims);
-//    }
-//
-//    public boolean isTokenValid(String token) {
-//        return isTokenExpired(token);
-//    }
-//
-//    private boolean isTokenExpired(String token) {
-//        return !extractExpiration(token).before(new Date());
-//    }
-//
-//    private Date extractExpiration(String token) {
-//        return extractClaim(token, Claims::getExpiration);
-//    }
-//
-//    private Claims extractAllClaims(String token) {
-//        return Jwts
-//                .parser()
-//                .verifyWith(getSigningKey())
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload();
-//    }
-//
-//    private SecretKey getSigningKey() {
-//        byte[] keyBytes = Base64.getDecoder().decode(secret);
-//        return Keys.hmacShaKeyFor(keyBytes);
-//    }
-//}
+package com.mariamkatamashvili.trainerwork.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token) {
+        return isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return !extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}

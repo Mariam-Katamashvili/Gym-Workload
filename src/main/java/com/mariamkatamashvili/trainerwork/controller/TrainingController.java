861f6687c27dc1a9530649f2522acbda31e5d1cb
MariamKatamashvili <katamashvili.mariam@kiu.edu.ge>
2024-05-25 20:55:35 +0400

    Add swagger

--- src/main/java/com/mariamkatamashvili/trainerwork/controller/TrainingController.java
+++ src/main/java/com/mariamkatamashvili/trainerwork/controller/TrainingController.java
@@ -3,6 +3,12 @@ package com.mariamkatamashvili.trainerwork.controller;
 import com.mariamkatamashvili.trainerwork.dto.WorkDTO;
 import com.mariamkatamashvili.trainerwork.dto.WorkloadDTO;
 import com.mariamkatamashvili.trainerwork.service.TrainersWorkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +22,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/trainings")
 @RequiredArgsConstructor
+@Tag(name = "Workload API", description = "Operations related to calculating and retrieving trainer working hours")
 public class TrainingController {
     private final TrainersWorkService trainersWorkService;
 
+    @Operation(summary = "Add workload to a trainer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Workload added successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping("/workload")
     public ResponseEntity<Void> workload(
             @RequestBody WorkloadDTO workload
@@ -27,6 +40,13 @@ public class TrainingController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Get all workloads")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of workloads retrieved successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = WorkDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @GetMapping
     public ResponseEntity<List<WorkDTO>> getAll() {
         return ResponseEntity.ok(trainersWorkService.findAll());
